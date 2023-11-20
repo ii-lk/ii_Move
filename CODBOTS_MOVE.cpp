285f99a26319746b25bc788f88796e2efb5bb5ec
Dileepa Jayawardena <me.dreamwalker@gmail.com>
2023-11-20 22:59:48 +0530

    demo app div

--- CODBOTS_MOVE.cpp
+++ CODBOTS_MOVE.cpp
@@ -1,21 +1,27 @@
 
 #include <CODBOTS_MOVE.h>
 
- CODBOTS_MOVE::CODBOTS_MOVE(){
-
- }
+CODBOTS_MOVE::CODBOTS_MOVE()
+{
+}
 // Implement the addMotor function
-int CODBOTS_MOVE::addMotor(MOTOR m) {
-    if (motorCount < MAX_MOTORS) {
+int CODBOTS_MOVE::addMotor(MOTOR m)
+{
+    if (motorCount < MAX_MOTORS)
+    {
         motors[motorCount] = m; // Add the motor to the array
-        motorCount++; // Increment the count
+        motorCount++;           // Increment the count
     }
     return motorCount; // Return the new count
 }
 
-void CODBOTS_MOVE::move(float m0, float m1) {
-    if (motorCount > 1) {
-        motors[0].move(m0);
-        motors[1].move(m1);
+void CODBOTS_MOVE::move(float m0_, float m1_)
+{
+    if (motorCount > 1)
+    {
+        motors[0].move(m0_);
+        motors[1].move(m1_);
+        m0 = m0_;
+        m1 = m1_;
     }
 }
