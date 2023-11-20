86554e539112c1e2711c4d51ab2adea69ea887e2
Dileepa Jayawardena <me.dreamwalker@gmail.com>
2023-11-20 12:08:15 +0530

    demo_app completed

--- CODBOTS_MOVE.h
+++ CODBOTS_MOVE.h
@@ -8,18 +8,19 @@
 
 #define MAX_MOTORS 10 // Define the maximum number of motors
 
-class CODBOTS_MOVE {
+class CODBOTS_MOVE
+{
 
 public:
     CODBOTS_MOVE();
     int addMotor(MOTOR m);
- 
+    float m0;
+    float m1;
     void move(float m0, float m1);
 
 private:
     MOTOR motors[MAX_MOTORS]; // Array of MOTOR objects
-    int motorCount = 0; // Keep track of the number of motors added
-    
+    int motorCount = 0;       // Keep track of the number of motors added
 };
 
 #endif
